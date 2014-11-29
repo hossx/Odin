f485c6a003bc65ce34c9003e77188ecee03b65b2
MaChao <chao@coinport.com>
2014-11-29 17:57:31 +0800

    adjust soft keyboard in buysell page(not finish yet)

---
+++ app/src/main/java/com/coinport/odin/BuySellLayout.java
@@ -0,0 +1,29 @@
+package com.coinport.odin;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+
+/**
+ * Created by hoss on 14-11-29.
+ */
+public class BuySellLayout extends LinearLayout {
+    public BuySellLayout(Context context) {
+        super(context);
+    }
+
+    public BuySellLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BuySellLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+//        this.setY(-200);
+    }
+}
