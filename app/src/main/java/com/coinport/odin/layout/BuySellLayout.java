b555936e1c6ef7119765d305b1238b7d8f5bbb46
MaChao <chao@coinport.com>
2014-12-04 21:10:59 +0800

    scroll panel to the bottom after soft keyboard popup

--- app/src/main/java/com/coinport/odin/layout/BuySellLayout.java
+++ app/src/main/java/com/coinport/odin/layout/BuySellLayout.java
@@ -2,8 +2,12 @@ package com.coinport.odin.layout;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import com.coinport.odin.R;
 
 /**
  * Created by hoss on 14-11-29.
@@ -24,7 +28,13 @@ public class BuySellLayout extends LinearLayout {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-//        System.out.println("kkkkkkkkkkkkkkkkkk");
-//        this.setY(-450);
+        final ScrollView sv = (ScrollView) findViewById(R.id.scroll_panel);
+        // TODO(c): scroll to top if hide the soft keyboard
+        sv.post(new Runnable() {
+            @Override
+            public void run() {
+                sv.scrollTo(0, sv.getBottom() + 400);
+            }
+        });
     }
 }
