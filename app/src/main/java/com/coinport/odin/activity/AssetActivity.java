b0f153c1fba7fd70afb9941ba46cc2bd0947a6ca
MaChao <chao@coinport.com>
2014-12-20 17:08:17 +0800

    user page. left: - get all asset as cny/btc - change pw - bind phone

--- app/src/main/java/com/coinport/odin/activity/AssetActivity.java
+++ app/src/main/java/com/coinport/odin/activity/AssetActivity.java
@@ -10,13 +10,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import com.coinport.odin.App;
 import com.coinport.odin.R;
+import com.coinport.odin.library.ptr.PullToRefreshBase;
+import com.coinport.odin.library.ptr.PullToRefreshScrollView;
+import com.coinport.odin.network.NetworkAsyncTask;
+import com.coinport.odin.network.NetworkRequest;
+import com.coinport.odin.network.OnApiResponseListener;
+import com.coinport.odin.obj.AccountInfo;
+import com.coinport.odin.util.Constants;
 import com.coinport.odin.util.Util;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,6 +35,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class AssetActivity extends Activity {
+    private ArrayList<HashMap<String, String>> assetItems = new ArrayList<>();
+    private SimpleAdapter adapter;
+    protected PullToRefreshScrollView refreshableView;
 
     private Typeface iconTF;
     @Override
@@ -36,33 +47,17 @@ public class AssetActivity extends Activity {
         setContentView(R.layout.activity_asset);
 
         iconTF = Typeface.createFromAsset(getAssets(), "coinport.ttf");
-
-        TextView sumCny = (TextView) findViewById(R.id.asset_sum_cny);
-        TextView sumBtc = (TextView) findViewById(R.id.asset_sum_btc);
-        sumCny.setText(String.format(getString(R.string.asset_sum_cny), "314324.2432"));
-        sumBtc.setText(String.format(getString(R.string.asset_sum_btc), "4324.2432"));
-
-        ListView lv = (ListView) findViewById(R.id.assets);
-        ArrayList<HashMap<String, String>> aiList = new ArrayList<>();
-        try {
-            JSONObject jsonObject = Util.getJsonObjectFromFile(this, "asset_mock.json").getJSONObject("data")
-                .getJSONObject("accounts");
-            Iterator<String> it = jsonObject.keys();
-            while (it.hasNext()) {
-                HashMap<String, String> fields = new HashMap<>();
-                JSONObject jsonObj = jsonObject.getJSONObject(it.next());
-                double pending = jsonObj.getJSONObject("locked").getDouble("value") +
-                    jsonObj.getJSONObject("pendingWithdrawal").getDouble("value");
-                fields.put("currency", jsonObj.getString("currency"));
-                fields.put("valid", jsonObj.getJSONObject("available").getString("display"));
-                fields.put("pending", (new BigDecimal(pending).setScale(4, RoundingMode.CEILING)).toPlainString());
-                aiList.add(fields);
+        refreshableView = (PullToRefreshScrollView) findViewById(R.id.refreshable_view);
+        refreshableView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ScrollView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ScrollView> refreshView) {
+                fetchAsset(true);
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        SimpleAdapter adapter = new SimpleAdapter(this, aiList, R.layout.asset_item, new String[]{
-            "currency", "valid", "pending"}, new int[] {R.id.asset_currency, R.id.asset_valid, R.id.asset_pending}) {
+        });
+
+        adapter = new SimpleAdapter(AssetActivity.this, assetItems, R.layout.asset_item, new String[]{
+                "currency", "valid", "pending"}, new int[] {
+                R.id.asset_currency, R.id.asset_valid, R.id.asset_pending}) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
@@ -75,9 +70,16 @@ public class AssetActivity extends Activity {
                 return view;
             }
         };
+
+        ListView lv = (ListView) findViewById(R.id.assets);
         lv.setAdapter(adapter);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fetchAsset(false);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -99,5 +101,46 @@ public class AssetActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void fetchAsset(final boolean isRefresh) {
+        AccountInfo ai = App.getAccount();
+        String url = String.format(Constants.ASSET_URL, ai.uid);
+        NetworkAsyncTask task = new NetworkAsyncTask(url, Constants.HttpMethod.GET)
+                .setOnSucceedListener(new OnApiResponseListener())
+                .setOnFailedListener(new OnApiResponseListener())
+                .setRenderListener(new NetworkAsyncTask.OnPostRenderListener() {
+                    @Override
+                    public void onRender(NetworkRequest s) {
+                        if (isRefresh)
+                            refreshableView.onRefreshComplete();
+                        if (s.getApiStatus() != NetworkRequest.ApiStatus.SUCCEED)
+                            return;
+                        TextView sumCny = (TextView) findViewById(R.id.asset_sum_cny);
+                        TextView sumBtc = (TextView) findViewById(R.id.asset_sum_btc);
+                        sumCny.setText(String.format(getString(R.string.asset_sum_cny), "314324.2432"));
+                        sumBtc.setText(String.format(getString(R.string.asset_sum_btc), "4324.2432"));
+
+                        try {
+                            JSONObject jsonObject = Util.getJsonObjectByPath(s.getApiResult(), "data.accounts");
+                            assetItems.clear();
+                            Iterator<String> it = jsonObject.keys();
+                            while (it.hasNext()) {
+                                HashMap<String, String> fields = new HashMap<>();
+                                JSONObject jsonObj = jsonObject.getJSONObject(it.next());
+                                double pending = jsonObj.getJSONObject("locked").getDouble("value") +
+                                        jsonObj.getJSONObject("pendingWithdrawal").getDouble("value");
+                                fields.put("currency", jsonObj.getString("currency"));
+                                fields.put("valid", jsonObj.getJSONObject("available").getString("display"));
+                                fields.put("pending", (new BigDecimal(pending).setScale(4, RoundingMode.CEILING)).toPlainString());
+                                assetItems.add(fields);
+                            }
+                            adapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        task.execute();
     }
 }
--- app/src/main/java/com/coinport/odin/fragment/UserFragment.java
+++ app/src/main/java/com/coinport/odin/fragment/UserFragment.java
@@ -6,39 +6,105 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.coinport.odin.App;
 import com.coinport.odin.R;
 import com.coinport.odin.activity.AssetActivity;
 import com.coinport.odin.activity.BindPhoneActivity;
 import com.coinport.odin.activity.ChangePwActivity;
+import com.coinport.odin.activity.LoginActivity;
 import com.coinport.odin.activity.ResetPwActivity;
 import com.coinport.odin.activity.UserVerifyActivity;
+import com.coinport.odin.obj.AccountInfo;
 
 public class UserFragment extends Fragment {
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.user_fragment, container, false);
+        view = inflater.inflate(R.layout.user_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initUi();
+    }
+
+    private void initUi() {
+        if (App.getAccount().uid == null) {
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            return;
+        }
+        AccountInfo info = App.getAccount();
+        String helloTo;
+        if (info.realname != null) {
+            helloTo = info.realname;
+        } else if (info.nickname != null) {
+            helloTo = info.nickname;
+        } else {
+            helloTo = info.username;
+        }
         TextView userName = (TextView) view.findViewById(R.id.user_name);
-        userName.setText("m_chao_1984@163.com");
+        userName.setText(helloTo);
+        TextView accountNumber = (TextView) view.findViewById(R.id.account_number);
+        accountNumber.setText(info.uid);
+        TextView accountName = (TextView) view.findViewById(R.id.account_name);
+        accountName.setText(info.username);
+        TextView accountPhone = (TextView) view.findViewById(R.id.account_phone);
+        if (info.mobileVerified) {
+            accountPhone.setText(info.mobile);
+        } else {
+            accountPhone.setText(R.string.phone_unverified);
+        }
+        TableRow userVerify = (TableRow) view.findViewById(R.id.user_verify);
+        if (info.realname != null) {
+            userVerify.setClickable(false);
+            ImageView usvi = (ImageView) view.findViewById(R.id.user_secure_verify_icon);
+            usvi.setVisibility(View.GONE);
+            TextView usv = (TextView) view.findViewById(R.id.user_secure_verified);
+            usv.setVisibility(View.VISIBLE);
+        } else {
+            userVerify.setClickable(true);
+            userVerify.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setClass(getActivity(), UserVerifyActivity.class);
+                    getActivity().startActivity(intent);
+                }
+            });
+        }
+        TableRow bindPhone = (TableRow) view.findViewById(R.id.bind_phone);
+        if (info.mobileVerified) {
+            bindPhone.setClickable(false);
+            ImageView usbpi = (ImageView) view.findViewById(R.id.user_secure_bind_phone_icon);
+            usbpi.setVisibility(View.GONE);
+            TextView uspb = (TextView) view.findViewById(R.id.user_secure_phone_bind);
+            uspb.setVisibility(View.VISIBLE);
+        } else {
+            bindPhone.setClickable(true);
+            bindPhone.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setClass(getActivity(), BindPhoneActivity.class);
+                    getActivity().startActivity(intent);
+                }
+            });
+        }
         LinearLayout userAsset = (LinearLayout) view.findViewById(R.id.user_asset);
         userAsset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(getActivity(), AssetActivity.class);
-                getActivity().startActivity(intent);
-            }
-        });
-        TableRow userVerify = (TableRow) view.findViewById(R.id.user_verify);
-        userVerify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(getActivity(), UserVerifyActivity.class);
                 getActivity().startActivity(intent);
             }
         });
@@ -51,15 +117,5 @@ public class UserFragment extends Fragment {
                 getActivity().startActivity(intent);
             }
         });
-        TableRow bindPhone = (TableRow) view.findViewById(R.id.bind_phone);
-        bindPhone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(getActivity(), BindPhoneActivity.class);
-                getActivity().startActivity(intent);
-            }
-        });
-        return view;
     }
 }
