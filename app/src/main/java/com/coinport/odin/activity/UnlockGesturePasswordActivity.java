f026f07087c379eb5f2950ef82bfcf62a7f273f6
MaChao <chao@coinport.com>
2015-01-14 15:22:13 +0800

    fix the slash pw unlocked after press back key

--- app/src/main/java/com/coinport/odin/activity/UnlockGesturePasswordActivity.java
+++ app/src/main/java/com/coinport/odin/activity/UnlockGesturePasswordActivity.java
@@ -1,187 +1,1 @@
-package com.coinport.odin.activity;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.os.Handler;
-import android.view.Gravity;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.coinport.odin.App;
-import com.coinport.odin.R;
-import com.coinport.odin.lock.LockPatternUtils;
-import com.coinport.odin.lock.LockPatternView;
-import com.coinport.odin.network.CookieDBManager;
-import com.coinport.odin.network.NetworkAsyncTask;
-import com.coinport.odin.network.NetworkRequest;
-import com.coinport.odin.util.Constants;
-import com.coinport.odin.util.Util;
-
-import java.util.List;
-
-public class UnlockGesturePasswordActivity extends Activity {
-	private LockPatternView mLockPatternView;
-	private int mFailedPatternAttemptsSinceLastTimeout = 0;
-	private CountDownTimer mCountdownTimer = null;
-	private Handler mHandler = new Handler();
-	private TextView mHeadTextView;
-	private Animation mShakeAnim;
-
-	private Toast mToast;
-
-	private void showToast(CharSequence message) {
-		if (null == mToast) {
-			mToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-			mToast.setGravity(Gravity.CENTER, 0, 0);
-		} else {
-			mToast.setText(message);
-		}
-
-		mToast.show();
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.gesturepassword_unlock);
-
-		mLockPatternView = (LockPatternView) this.findViewById(R.id.gesturepwd_unlock_lockview);
-		mLockPatternView.setOnPatternListener(mChooseNewLockPatternListener);
-		mLockPatternView.setTactileFeedbackEnabled(true);
-		mHeadTextView = (TextView) findViewById(R.id.gesturepwd_unlock_text);
-		mShakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_x);
-
-        Button forgetPw = (Button) findViewById(R.id.gesturepwd_unlock_forget);
-        forgetPw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NetworkAsyncTask task = new NetworkAsyncTask(Constants.LOGOUT_URL, Constants.HttpMethod.GET)
-                        .setRenderListener(new NetworkAsyncTask.OnPostRenderListener() {
-                            @Override
-                            public void onRender(NetworkRequest s) {
-                                App.setSetGesturePw(false);
-                                Intent intent = Util.toLoginFromAuthFail(UnlockGesturePasswordActivity.this, false);
-                                UnlockGesturePasswordActivity.this.startActivity(intent);
-                                UnlockGesturePasswordActivity.this.finish();
-                                showToast(getString(R.string.gesture_cancel_hint));
-                            }
-                        });
-                task.execute();
-            }
-        });
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		if (!App.getLockPatternUtils().savedPatternExists()) {
-			startActivity(new Intent(this, GuideGesturePasswordActivity.class));
-			finish();
-		}
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (mCountdownTimer != null)
-			mCountdownTimer.cancel();
-	}
-	private Runnable mClearPatternRunnable = new Runnable() {
-		public void run() {
-			mLockPatternView.clearPattern();
-		}
-	};
-
-	protected LockPatternView.OnPatternListener mChooseNewLockPatternListener = new LockPatternView.OnPatternListener() {
-
-		public void onPatternStart() {
-			mLockPatternView.removeCallbacks(mClearPatternRunnable);
-			patternInProgress();
-		}
-
-		public void onPatternCleared() {
-			mLockPatternView.removeCallbacks(mClearPatternRunnable);
-		}
-
-		public void onPatternDetected(List<LockPatternView.Cell> pattern) {
-			if (pattern == null)
-				return;
-			if (App.getLockPatternUtils().checkPattern(pattern)) {
-				mLockPatternView.setDisplayMode(LockPatternView.DisplayMode.Correct);
-                if (!App.isMainActivityCreated()) {
-                    Intent intent = Util.toMainOrLogin(UnlockGesturePasswordActivity.this);
-                    UnlockGesturePasswordActivity.this.startActivity(intent);
-                }
-                finish();
-			} else {
-				mLockPatternView.setDisplayMode(LockPatternView.DisplayMode.Wrong);
-				if (pattern.size() >= LockPatternUtils.MIN_PATTERN_REGISTER_FAIL) {
-					mFailedPatternAttemptsSinceLastTimeout++;
-					int retry = LockPatternUtils.FAILED_ATTEMPTS_BEFORE_TIMEOUT
-							- mFailedPatternAttemptsSinceLastTimeout;
-					if (retry >= 0) {
-						if (retry == 0)
-							showToast("您已5次输错密码，请30秒后再试");
-						mHeadTextView.setText("密码错误，还可以再输入" + retry + "次");
-						mHeadTextView.setTextColor(Color.RED);
-						mHeadTextView.startAnimation(mShakeAnim);
-					}
-
-				}else{
-					showToast("输入长度不够，请重试");
-				}
-
-				if (mFailedPatternAttemptsSinceLastTimeout >= LockPatternUtils.FAILED_ATTEMPTS_BEFORE_TIMEOUT) {
-					mHandler.postDelayed(attemptLockout, 2000);
-				} else {
-					mLockPatternView.postDelayed(mClearPatternRunnable, 2000);
-				}
-			}
-		}
-
-		public void onPatternCellAdded(List<LockPatternView.Cell> pattern) {
-
-		}
-
-		private void patternInProgress() {
-		}
-	};
-
-	Runnable attemptLockout = new Runnable() {
-
-		@Override
-		public void run() {
-			mLockPatternView.clearPattern();
-			mLockPatternView.setEnabled(false);
-			mCountdownTimer = new CountDownTimer(
-					LockPatternUtils.FAILED_ATTEMPT_TIMEOUT_MS + 1, 1000) {
-
-				@Override
-				public void onTick(long millisUntilFinished) {
-					int secondsRemaining = (int) (millisUntilFinished / 1000) - 1;
-					if (secondsRemaining > 0) {
-						mHeadTextView.setText(secondsRemaining + " 秒后重试");
-					} else {
-						mHeadTextView.setText("请绘制手势密码");
-						mHeadTextView.setTextColor(Color.WHITE);
-					}
-
-				}
-
-				@Override
-				public void onFinish() {
-					mLockPatternView.setEnabled(true);
-					mFailedPatternAttemptsSinceLastTimeout = 0;
-				}
-			}.start();
-		}
-	};
-}
+package com.coinport.odin.activity;import android.app.Activity;import android.content.Intent;import android.graphics.Color;import android.os.Bundle;import android.os.CountDownTimer;import android.os.Handler;import android.view.Gravity;import android.view.KeyEvent;import android.view.View;import android.view.animation.Animation;import android.view.animation.AnimationUtils;import android.widget.Button;import android.widget.TextView;import android.widget.Toast;import com.coinport.odin.App;import com.coinport.odin.R;import com.coinport.odin.lock.LockPatternUtils;import com.coinport.odin.lock.LockPatternView;import com.coinport.odin.network.CookieDBManager;import com.coinport.odin.network.NetworkAsyncTask;import com.coinport.odin.network.NetworkRequest;import com.coinport.odin.util.Constants;import com.coinport.odin.util.Util;import java.util.List;public class UnlockGesturePasswordActivity extends Activity {	private LockPatternView mLockPatternView;	private int mFailedPatternAttemptsSinceLastTimeout = 0;	private CountDownTimer mCountdownTimer = null;	private Handler mHandler = new Handler();	private TextView mHeadTextView;	private Animation mShakeAnim;	private Toast mToast;	private void showToast(CharSequence message) {		if (null == mToast) {			mToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);			mToast.setGravity(Gravity.CENTER, 0, 0);		} else {			mToast.setText(message);		}		mToast.show();	}	@Override	public void onCreate(Bundle savedInstanceState) {		super.onCreate(savedInstanceState);		setContentView(R.layout.gesturepassword_unlock);		mLockPatternView = (LockPatternView) this.findViewById(R.id.gesturepwd_unlock_lockview);		mLockPatternView.setOnPatternListener(mChooseNewLockPatternListener);		mLockPatternView.setTactileFeedbackEnabled(true);		mHeadTextView = (TextView) findViewById(R.id.gesturepwd_unlock_text);		mShakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_x);        Button forgetPw = (Button) findViewById(R.id.gesturepwd_unlock_forget);        forgetPw.setOnClickListener(new View.OnClickListener() {            @Override            public void onClick(View v) {                NetworkAsyncTask task = new NetworkAsyncTask(Constants.LOGOUT_URL, Constants.HttpMethod.GET)                        .setRenderListener(new NetworkAsyncTask.OnPostRenderListener() {                            @Override                            public void onRender(NetworkRequest s) {                                App.setSetGesturePw(false);                                Intent intent = Util.toLoginFromAuthFail(UnlockGesturePasswordActivity.this, false);                                UnlockGesturePasswordActivity.this.startActivity(intent);                                UnlockGesturePasswordActivity.this.finish();                                showToast(getString(R.string.gesture_cancel_hint));                            }                        });                task.execute();            }        });	}	@Override	protected void onResume() {		super.onResume();		if (!App.getLockPatternUtils().savedPatternExists()) {			startActivity(new Intent(this, GuideGesturePasswordActivity.class));			finish();		}	}	@Override	protected void onDestroy() {		super.onDestroy();		if (mCountdownTimer != null)			mCountdownTimer.cancel();	}	private Runnable mClearPatternRunnable = new Runnable() {		public void run() {			mLockPatternView.clearPattern();		}	};    @Override    public boolean onKeyDown(int keyCode, KeyEvent event) {        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN)        {            return true;        }        return super.onKeyDown(keyCode, event);    }	protected LockPatternView.OnPatternListener mChooseNewLockPatternListener = new LockPatternView.OnPatternListener() {		public void onPatternStart() {			mLockPatternView.removeCallbacks(mClearPatternRunnable);			patternInProgress();		}		public void onPatternCleared() {			mLockPatternView.removeCallbacks(mClearPatternRunnable);		}		public void onPatternDetected(List<LockPatternView.Cell> pattern) {			if (pattern == null)				return;			if (App.getLockPatternUtils().checkPattern(pattern)) {				mLockPatternView.setDisplayMode(LockPatternView.DisplayMode.Correct);                if (!App.isMainActivityCreated()) {                    Intent intent = Util.toMainOrLogin(UnlockGesturePasswordActivity.this);                    UnlockGesturePasswordActivity.this.startActivity(intent);                }                finish();			} else {				mLockPatternView.setDisplayMode(LockPatternView.DisplayMode.Wrong);				if (pattern.size() >= LockPatternUtils.MIN_PATTERN_REGISTER_FAIL) {					mFailedPatternAttemptsSinceLastTimeout++;					int retry = LockPatternUtils.FAILED_ATTEMPTS_BEFORE_TIMEOUT							- mFailedPatternAttemptsSinceLastTimeout;					if (retry >= 0) {						if (retry == 0)							showToast("您已5次输错密码，请30秒后再试");						mHeadTextView.setText("密码错误，还可以再输入" + retry + "次");						mHeadTextView.setTextColor(Color.RED);						mHeadTextView.startAnimation(mShakeAnim);					}				}else{					showToast("输入长度不够，请重试");				}				if (mFailedPatternAttemptsSinceLastTimeout >= LockPatternUtils.FAILED_ATTEMPTS_BEFORE_TIMEOUT) {					mHandler.postDelayed(attemptLockout, 2000);				} else {					mLockPatternView.postDelayed(mClearPatternRunnable, 2000);				}			}		}		public void onPatternCellAdded(List<LockPatternView.Cell> pattern) {		}		private void patternInProgress() {		}	};	Runnable attemptLockout = new Runnable() {		@Override		public void run() {			mLockPatternView.clearPattern();			mLockPatternView.setEnabled(false);			mCountdownTimer = new CountDownTimer(					LockPatternUtils.FAILED_ATTEMPT_TIMEOUT_MS + 1, 1000) {				@Override				public void onTick(long millisUntilFinished) {					int secondsRemaining = (int) (millisUntilFinished / 1000) - 1;					if (secondsRemaining > 0) {						mHeadTextView.setText(secondsRemaining + " 秒后重试");					} else {						mHeadTextView.setText("请绘制手势密码");						mHeadTextView.setTextColor(Color.WHITE);					}				}				@Override				public void onFinish() {					mLockPatternView.setEnabled(true);					mFailedPatternAttemptsSinceLastTimeout = 0;				}			}.start();		}	};}
