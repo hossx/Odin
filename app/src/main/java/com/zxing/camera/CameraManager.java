e048d1560eb879dc5e1fd154eb8f52e16a910225
MaChao <chao@coinport.com>
2015-02-11 20:04:27 +0800

    fix bug of bad scale since mis-use the width and height

--- app/src/main/java/com/zxing/camera/CameraManager.java
+++ app/src/main/java/com/zxing/camera/CameraManager.java
@@ -16,8 +16,8 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 480;
-  private static final int MIN_FRAME_HEIGHT = 480;
+  private static final int MIN_FRAME_WIDTH = 120;
+  private static final int MIN_FRAME_HEIGHT = 120;
   private static final int MAX_FRAME_WIDTH = 1536;
   private static final int MAX_FRAME_HEIGHT = 2048;
 
@@ -227,10 +227,10 @@ public final class CameraManager {
       Rect rect = new Rect(getFramingRect());
       Point cameraResolution = configManager.getCameraResolution();
       Point screenResolution = configManager.getScreenResolution();
-      rect.left = rect.left * cameraResolution.x / screenResolution.x;
-      rect.right = rect.right * cameraResolution.x / screenResolution.x;
-      rect.top = rect.top * cameraResolution.y / screenResolution.y;
-      rect.bottom = rect.bottom * cameraResolution.y / screenResolution.y;
+      rect.left = rect.left * cameraResolution.y / screenResolution.y;
+      rect.right = rect.right * cameraResolution.y / screenResolution.y;
+      rect.top = rect.top * cameraResolution.x / screenResolution.x;
+      rect.bottom = rect.bottom * cameraResolution.x / screenResolution.x;
       framingRectInPreview = rect;
     }
     return framingRectInPreview;
